d45f2fe1f92b6cd4d0c0028f4bd6fd8c26aae817
pritam hinger <pritamgenius24@gmail.com>
2018-01-21 12:25:48 +0530

    Adding Input Accessroy View to allow user to add comments.

--- Instasnaps/CommentController.swift
+++ Instasnaps/CommentController.swift
@@ -10,8 +10,54 @@ import UIKit
 
 class CommentController: UICollectionViewController {
     
+    let containerView: UIView = {
+        let commentView = UIView()
+        commentView.backgroundColor = .white
+        commentView.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        
+        let submitButton = UIButton(type: .system)
+        submitButton.setTitle("Submit", for: .normal)
+        submitButton.setTitleColor(.black, for: .normal)
+        submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        submitButton.addTarget(self, action: #selector(handleCommentSubmit), for: .touchUpInside)
+        commentView.addSubview(submitButton)
+        
+        let commentTextField = UITextField()
+        commentTextField.placeholder = "Enter your comment here..!!"
+        commentView.addSubview(commentTextField)
+        
+        submitButton.anchor(top: commentView.topAnchor, left: nil, bottom: commentView.bottomAnchor, right: commentView.rightAnchor, topPadding: 0, leftPadding: 0, bottomPadding: 0, rightPadding: 12, width: 50, height: 0)
+        commentTextField.anchor(top: commentView.topAnchor, left: commentView.leftAnchor, bottom: commentView.bottomAnchor, right: submitButton.leftAnchor, topPadding: 0, leftPadding: 12, bottomPadding: 0, rightPadding: 0, width: 0, height: 0)
+        return commentView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "Comments"
         collectionView?.backgroundColor = .purple
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    override var inputAccessoryView: UIView?{
+        get{
+            return containerView
+        }
+    }
+    
+    override var canBecomeFirstResponder: Bool{
+        return true
+    }
+    
+    @objc fileprivate func handleCommentSubmit(){
+        print("Submitting comment")
     }
 }
